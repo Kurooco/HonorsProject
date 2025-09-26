188680bf0ece6d165234bccbd8aaa650ef62c4fe
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-09-26 16:47:23 -0500

    fixed random spawner

--- scenes/level_objects/assist/food_spawner_copy.gd
+++ scenes/level_objects/assist/food_spawner_copy.gd
@@ -1,6 +1,6 @@
 extends Path2D
 
-@export var spawn_time = 0.4
+@export var spawn_time = .5
 @export var y_range = Vector2(-300, -100)
 @export var x_range = Vector2(-30, 30)
 @export var spawn_item = preload("res://scenes/level_objects/assist/food.tscn")
@@ -13,8 +13,10 @@ var start_point = 0
 var end_point = 0
 var adjusted_distance_ratio = 0
 
+var current_time = 0
+
 func _ready():
-	$Timer.wait_time = spawn_time
+	pass
 
 func _process(delta):
 	start_point = curve.get_baked_points()[0].x+position.x
@@ -23,12 +25,12 @@ func _process(delta):
 	start_x = max(start_point, get_viewport().get_camera_2d().position.x-get_viewport().size.x/2)
 	end_x = min(end_point, get_viewport().get_camera_2d().position.x+get_viewport().size.x/2)
 	adjusted_distance_ratio = (end_x-start_x)/get_viewport().size.x
-	$Timer.wait_time = spawn_time / adjusted_distance_ratio
 
-func _on_timer_timeout():
-	if(adjusted_distance_ratio <= 0 || disabled):
+	current_time += delta
+	if(adjusted_distance_ratio <= 0 || disabled || current_time < spawn_time/adjusted_distance_ratio):
 		return;
 	
+	current_time = 0
 	$Point1.global_position.x = start_x
 	$Point2.global_position.x = end_x
 	
