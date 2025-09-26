9a843df8ca75ab06853151e2e54a928649aede48
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-09-26 16:22:35 -0500

    made basic obstacle indicators

--- arc_mover.gd
+++ arc_mover.gd
@@ -12,5 +12,5 @@ func _ready():
 func _process(delta):
 	velocity.y += delta*GRAVITY
 	move_object.position += velocity*delta
-	if(move_object.position.y > birth_y):
+	if(velocity.y > 0 && move_object.position.y > get_viewport().get_camera_2d().position.y+get_viewport_rect().size.y/2):
 		move_object.queue_free()
--- scenes/level_objects/hazards/single_shooter.gd
+++ scenes/level_objects/hazards/single_shooter.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 @export var scene : PackedScene
 @export var velocity : Vector2
+@export var delay = 1.0
 
+func _ready():
+	$Timer.wait_time = delay
+	create_curve()
 
 func get_arc_mover(node: Node):
 	for child in node.get_children(true):
@@ -11,8 +15,18 @@ func get_arc_mover(node: Node):
 	return null
 
 func _on_timer_timeout():
-	var new_scene = scene.instantiate()
-	var arc_mover = get_arc_mover(new_scene)
-	new_scene.global_position = global_position
-	arc_mover.velocity = velocity
-	Autoload.level_handler.current_level.add_child(new_scene)
+	if(abs(get_viewport().get_camera_2d().position.x - position.x) < get_viewport_rect().size.x):
+		var new_scene = scene.instantiate()
+		var arc_mover = get_arc_mover(new_scene)
+		new_scene.global_position = global_position
+		arc_mover.velocity = velocity
+		Autoload.level_handler.current_level.add_child(new_scene)
+
+func create_curve():
+	var v = velocity
+	var gravity = 500
+	var pos = Vector2.ZERO
+	for i in range(200):
+		$Line2D.add_point(pos)
+		v.y += gravity*.01
+		pos += v*.01
