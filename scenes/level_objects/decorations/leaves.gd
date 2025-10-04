a999b81d438bdd1603aec8da98cfe1e1f0b39701
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-10-04 16:57:49 -0500

    made tree area

---
+++ scenes/level_objects/decorations/leaves.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+var rotate_offset
+var rotate_timer = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rotate_offset = randf_range(0, PI)
+	seed(position.length())
+	var size = randf_range(1, 2)
+	scale = Vector2(size, size)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate_timer += delta
+	rotation = sin(rotate_timer/2.0 + rotate_offset)/2.0 + rotate_offset
