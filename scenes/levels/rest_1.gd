6568c34b1ee603d28ac98cf1ea8959561d118fd1
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-10-04 18:40:45 -0500

    added wood texture

--- scenes/levels/rest_1.gd
+++ scenes/levels/rest_1.gd
@@ -8,6 +8,10 @@ func _ready():
 			var col = child.get_children()[0]
 			var shape = Polygon2D.new()
 			shape.polygon = col.polygon
+			#shape.color = Color.SADDLE_BROWN
+			var t : Texture = load("res://art/wood.png")
+			shape.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+			shape.texture = t
 			child.add_child(shape)
 	#$StaticBody2D/Polygon2D.polygon = $StaticBody2D/CollisionPolygon2D.polygon
 
