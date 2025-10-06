61d2704769496aea36a1bbd19c32c02e2cd24456
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-10-06 14:48:20 -0500

    created upgrade panel

--- scenes/level_objects/assist/coin.gd
+++ scenes/level_objects/assist/coin.gd
@@ -1,7 +1,8 @@
 extends "res://scenes/player/collectable.gd"
 
+@export var value = 1
 
 
 func _on_collected():
-	$PointAwarder.award_points(1)
+	$PointAwarder.award_points(value)
 	queue_free()
---
+++ scenes/ui/upgrade_menu/upgrade_panel.gd
@@ -0,0 +1,36 @@
+extends PanelContainer
+
+@export var title : String
+@export var icon : Texture
+@export var description : String
+@export var upgrade_var_name : String
+@export var cost_progrssion : Array[int]
+var upgrade_amount
+var upgrades = 0
+var progress_tween : Tween
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	upgrade_amount = cost_progrssion.size()
+	%Title.text = title
+	#%Icon.texture = icon
+	%Progress.texture_under = icon
+	%Progress.texture_progress = icon
+	%Description.text = description
+	update_display()
+
+
+func update_display():
+	if(is_instance_valid(progress_tween)):
+		progress_tween.kill()
+	progress_tween = create_tween()
+	progress_tween.tween_property(%Progress, "value", (float(upgrades)/upgrade_amount)*100, .5).set_trans(Tween.TRANS_QUART)
+	if(upgrades < upgrade_amount):
+		%Upgrade.text = str(cost_progrssion[upgrades]) + " SP"
+	else:
+		%Upgrade.text = "MAX"
+		%Upgrade.disabled = true
+
+func _on_upgrade_pressed():
+	upgrades += 1
+	update_display()
