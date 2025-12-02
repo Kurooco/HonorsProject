e9565382f4ce4438dbb7b5ce8dc167e4c6b3c296
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-12-02 10:33:34 -0500

    multiple followers on line follower

--- scenes/level_objects/auxiliary/line_follower.gd
+++ scenes/level_objects/auxiliary/line_follower.gd
@@ -10,23 +10,33 @@ enum MovementType
 @export var number = 1
 @export var speed = 10.0
 @export var movement_type : MovementType
-var follower : PathFollow2D
+var followers : Array[PathFollow2D]
 
 func _ready():
 	var path = Path2D.new()
 	var c = Curve2D.new()
 	for point in points:
 		c.add_point(point)
+	if(closed):
+		c.add_point(points[0])
 	path.curve = c
 	add_child(path)
 	for i in range(number):
-		follower = PathFollow2D.new()
+		var follower = PathFollow2D.new()
 		follower.rotates = false
 		follower.add_child(scene.instantiate())
+		followers.append(follower)
 		path.add_child(follower)
 	
 func _process(delta):
-	if(movement_type == MovementType.SIN):
-		follower.progress_ratio = .5 + cos((Time.get_ticks_msec()/5000.0)*speed)/2.0
-	else:
-		follower.progress_ratio = ((Time.get_ticks_msec()/5000.0)*speed)
+	var ind = 0
+	for follower in followers:
+		var offset
+		if(movement_type == MovementType.SIN):
+			offset = 2*PI*ind/followers.size()
+			follower.progress_ratio = .5 + cos((Time.get_ticks_msec()/5000.0)*speed + offset)/2.0
+		else:
+			offset = float(ind)/followers.size()
+			print_debug(offset)
+			follower.progress_ratio = ((Time.get_ticks_msec()/5000.0)*speed) + offset
+		ind += 1
