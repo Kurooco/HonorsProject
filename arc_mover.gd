10fb0a3b960b46e63826ad6e5f081cebca32a347
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-12-10 19:50:55 -0500

    ready to update spawners

--- arc_mover.gd
+++ arc_mover.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name ArcMover
 
 @export var move_object : Node
+@export var disabled = false
 const GRAVITY = 500
 var velocity : Vector2
 var birth_y : float
@@ -10,8 +11,9 @@ func _ready():
 	birth_y = move_object.position.y
 
 func _process(delta):
-	velocity.y += delta*GRAVITY
-	move_object.position += velocity*delta
-	var camera = get_viewport().get_camera_2d()
-	if(velocity.y > 0 && move_object.position.y > camera.position.y+get_viewport_rect().size.y/camera.zoom.x/2):
-		move_object.queue_free()
+	if(!disabled):
+		velocity.y += delta*GRAVITY
+		move_object.position += velocity*delta
+		var camera = get_viewport().get_camera_2d()
+		if(velocity.y > 0 && move_object.position.y > camera.position.y+get_viewport_rect().size.y/camera.zoom.x/2):
+			move_object.queue_free()
--- scenes/level_objects/assist/food.gd
+++ scenes/level_objects/assist/food.gd
@@ -1,7 +1,6 @@
 extends Area2D
 class_name Food
 
-
 func jump_on():
 	$ArcMover.velocity.y = 700
 	set_deferred("monitorable", false)
