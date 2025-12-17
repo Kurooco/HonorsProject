8a82e6beecf9bb3f6298307aa77cecf41dbb4e63
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-12-16 19:29:05 -0500

    added character sprites

--- globals/MusicHandler.gd
+++ globals/MusicHandler.gd
@@ -2,7 +2,7 @@ extends Node
 
 var current_song_path = ""
 var current_song_node : AudioStreamPlayer = null
-var play_music = true
+var play_music = false
 
 func play(path:String):
 	if(!play_music):
