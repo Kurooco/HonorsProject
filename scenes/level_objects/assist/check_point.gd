7d1e8a69218ef0ebda8887b4f100de153727942f
David Sulpice <DJsulpice123@gmail.com>
2026-02-07 09:26:42 -0600

    added font, checkpoint label

--- scenes/level_objects/assist/check_point.gd
+++ scenes/level_objects/assist/check_point.gd
@@ -30,6 +30,18 @@ func claim(effects:bool):
 	if(effects):
 		bird_killed = true
 		$AudioStreamPlayer2D.play()
+		$Label.modulate = Color.TRANSPARENT
+		$Label.show()
+		var t : Tween = create_tween()
+		t.set_trans(Tween.TRANS_SINE)
+		t.tween_property($Label, "position", $Label.position+Vector2(0, -50), .5)
+		t.parallel().tween_property($Label, "modulate", Color.WHITE, .5)
+		await t.finished
+		await get_tree().create_timer(1).timeout
+		t = create_tween()
+		t.set_trans(Tween.TRANS_SINE)
+		t.tween_property($Label, "position", $Label.position+Vector2(0, 50), .5)
+		t.parallel().tween_property($Label, "modulate", Color.TRANSPARENT, .5)
 	else:
 		$Bird.hide()
 		disabled = true
