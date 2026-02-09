9d6565c8c3ae5b11e9914ca279e555b3134479cd
David Sulpice <DJsulpice123@gmail.com>
2026-02-09 12:50:41 -0600

    added windy plant

--- scenes/level_objects/interactive/flower.gd
+++ scenes/level_objects/interactive/flower.gd
@@ -6,16 +6,19 @@ var sway_tween : Tween = null
 var initial_rotation
 var sway_amount = 0.0
 @export var min_sway = 0.0
+@export var min_sway_speed = 1.0
+@export var max_sway_speed = 8.0
 
 func _ready():
 	initial_rotation = rotation
+	sway_speed = min_sway_speed
 
 func _on_entered():
 	sway_tween = create_tween()
 	sway_tween.tween_property(self, "sway", .8, .1)
-	sway_tween.parallel().tween_property(self, "sway_speed", 8.0, .1)
+	sway_tween.parallel().tween_property(self, "sway_speed", max_sway_speed, .1)
 	sway_tween.tween_property(self, "sway", 0.0, 2).set_trans(Tween.TRANS_CUBIC)
-	sway_tween.parallel().tween_property(self, "sway_speed", 1.0, 4).set_trans(Tween.TRANS_CUBIC)
+	sway_tween.parallel().tween_property(self, "sway_speed", min_sway_speed, 4).set_trans(Tween.TRANS_CUBIC)
 
 func _process(delta):
 	sway_amount += delta*sway_speed
