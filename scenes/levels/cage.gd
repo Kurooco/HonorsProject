839440ec45f16c8bfa47a682a77cc008c823b4fd
David Sulpice <DJsulpice123@gmail.com>
2026-03-08 14:29:47 -0500

    generalized cage, need to fix collision

--- scenes/levels/cage.gd
+++ scenes/levels/cage.gd
@@ -3,6 +3,15 @@ extends Sprite2D
 #var class_of_object = "Sprite2D"
 @onready var bottom_collision: CollisionShape2D = $Bottom/BottomCollision
 @onready var door: StaticBody2D = $Door
+var loaded = false
+
+signal finished_moving
+
+func _ready():
+	if(loaded):
+		$DoorCloser.disabled = false
+		set_bottom(false)
+		open_door(true)
 
 func set_bottom(b):
 	bottom_collision.set_deferred("disabled", b)
@@ -13,3 +22,29 @@ func open_door(b):
 		t.tween_property(door, "position:y", 500, .2)
 	else:
 		t.tween_property(door, "position:y", 0, .2)
+
+func on_load():
+	loaded = true
+
+func _on_door_closer_activated() -> void:
+	open_door(false)
+	await get_tree().create_timer(1).timeout
+	if(global_position.y > 0):
+		move_up(10)
+	else:
+		move_down(10)
+
+func move_down(time=1.0):
+	move(152, time)
+
+func move_up(time=1.0):
+	move(-1176, time)
+
+func move(dest, time=1.0):
+	var t : Tween = create_tween()
+	t.tween_property(self, "global_position:y", dest, time)
+	t.finished.connect(finish_moving)
+
+func finish_moving():
+	finished_moving.emit()
+	
--- scenes/levels/ending_interior.gd
+++ scenes/levels/ending_interior.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 @onready var cage: Sprite2D = $Cage
 @onready var camera: Camera2D = $Camera2D
+var loaded = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if(loaded):
+		$Cheese.queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,14 +17,15 @@ func _process(delta: float) -> void:
 
 func _on_interaction_area_activated() -> void:
 	camera.focus_on_player(Vector2(2, 2), 10)
-	var t : Tween = create_tween()
-	t.tween_property(cage, "global_position:y", 152, .2)
-	t.finished.connect(cage.set_bottom.bind(false))
+	cage.move_down(.2)
 	await get_tree().create_timer(2).timeout
 	Dialogic.start("caged")
 	await $DialogicSignalHandler.matched
 	await get_tree().create_timer(2).timeout
-	t = create_tween()
-	t.tween_property(cage, "global_position:y", -1192, 10)
-	await t.finished
+	cage.set_bottom(false)
+	cage.move_up(10)
+	await cage.finished_moving
 	cage.open_door(true)
+
+func on_load():
+	loaded = true
