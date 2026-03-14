3200f712e24c438e2d791ed29d78b654f1b87546
David Sulpice <DJsulpice123@gmail.com>
2026-03-14 14:09:04 -0500

    added birds to dream, made trippy

--- scenes/levels/dorm/dream.gd
+++ scenes/levels/dorm/dream.gd
@@ -11,9 +11,16 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	color_rect.set_instance_shader_parameter("camera_pos", camera_2d.get_screen_center_position())
-
+	
 
 func wake_up(area):
 	AudioServer.set_bus_layout(load("res://sound/bus_layouts/default_bus_layout.tres"))
 	PlayerStats.nighttime = false
 	Autoload.level_handler.set_level_with_spawn_point("res://scenes/levels/dorm/dorm_room2.tscn", "bed", false)
+
+
+func _on_bird_timer_timeout() -> void:
+	var bx = randi_range(0, 1000)
+	var bird = load("res://experimentation/flyer.tscn").instantiate()
+	bird.position = Vector2(bx, 800)
+	add_child(bird)
