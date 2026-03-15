a8f1c9a90a431c64adf5c4c112d40cb0127bbbd3
David Sulpice <DJsulpice123@gmail.com>
2026-03-15 17:17:46 -0500

    added duck

---
+++ scenes/levels/dorm/dorm_room_base.gd
@@ -0,0 +1,15 @@
+extends "res://scenes/levels/rest_1.gd"
+
+@export var deleted_nodes_during_day : Array[Node]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super()
+	if(!PlayerStats.nighttime):
+		for node in deleted_nodes_during_day:
+			node.queue_free()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
